cace5e9e7bff460420c5a499ca0d8004c73ffbd8
mhoutreille <mhoutreille@pictet-technologies.com>
2023-10-13 11:29:26 +0200

    chore(STATUS): add status service

---
+++ health-service/src/main/java/org/eblood/healthservice/controller/SayHelloController.java
@@ -0,0 +1,13 @@
+package org.eblood.healthservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SayHelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello !";
+    }
+}
