0474b609fcba5b233d805166abec51538c64f508
mhoutreille <mhoutreille@pictet-technologies.com>
2023-12-08 16:30:16 +0100

    feat(MKD): add health service helm chart

--- health-service/src/main/java/org/eblood/healthservice/controller/SayHelloController.java
+++ health-service/src/main/java/org/eblood/healthservice/controller/SayHelloController.java
@@ -1,6 +1,5 @@
 package org.eblood.healthservice.controller;
 
-import io.swagger.client.api.FxSpotApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SayHelloController {
 
 
-    FxSpotApi fxSpotApi = null;
 
     @GetMapping("/hello")
     public String sayHello() {
